1a4d56149f6215be88371262216db7efa00b57c0
Hugo Landeros <hugolanderos7@outlook.com>
2022-10-16 09:35:02 -0500

    Create name inputs

---
+++ love-calculator.py
@@ -0,0 +1,9 @@
+print("Welcome to love calculator\n")
+
+name1 = input("What is your name?\n")
+
+name2 = input("What is their name?\n")
+
+print(f"\nYour name is {name1}\n")
+
+print(f"Their name is {name2}\n")
