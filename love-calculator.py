2bf231b2fda9d2dff81958e132f51d6c9051c207
Hugo Landeros <hugolanderos7@outlook.com>
2022-10-16 13:58:43 -0500

    Finish porcentage calculation

--- love-calculator.py
+++ love-calculator.py
@@ -1,9 +1,32 @@
 print("Welcome to love calculator\n")
 
-name1 = input("What is your name?\n")
+name1 = input("What is your name?\n").lower()
+name2 = input("What is their name?\n").lower()
+score1 = 0
+score2 = 0
 
-name2 = input("What is their name?\n")
+# Concatenate both names variables
+names = name1 + name2
 
-print(f"\nYour name is {name1}\n")
+# Score1 (TRUE) incrementation
+score1+=names.count("t")
+score1+=names.count("r")
+score1+=names.count("u")
+score1+=names.count("e")
 
-print(f"Their name is {name2}\n")
+# Score2 (LOVE) incrementation
+score2+=names.count("l")
+score2+=names.count("o")
+score2+=names.count("v")
+score2+=names.count("e")
+
+# Convert scores variables to strings
+score1 = str(score1)
+score2 = str(score2)
+
+# Concatenate scores variables
+porcentage =  score1 + score2
+
+print(f"You are {porcentage}% compatible")
+
+
